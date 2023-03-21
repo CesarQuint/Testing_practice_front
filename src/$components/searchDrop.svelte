ec5cf6f927c55ce47cfdd2a3b32ccb97c12ef7e8
CesarQuint <pedrocesarquintero001@gmail.com>
2023-03-20 22:05:25 -0600

    getting and setting users

--- src/$components/searchDrop.svelte
+++ src/$components/searchDrop.svelte
@@ -1,45 +1,56 @@
 <script>
     
     import { createEventDispatcher } from 'svelte'
+    import { UsersStore, UserStore, ToastStore } from '../stores'
     import { fade } from 'svelte/transition'
 
+    import UsersService from '../$services/users.service'
     import Input from './input.svelte'
+  import User from '../users/user.read.svelte';
+
+    export let value = null
+    let query = {find:value}
+    $:value =query.find=value
+    let metadata = {}
 
     const dispatch = createEventDispatcher()
 
+    async function getUsers(){
+      console.log(value.length);
+      if (value.length < 2) {
+        return
+      }
+      const response = await UsersService.getUsers(query)
+      if(response.error)
+            return ToastStore.error(response.error)
+
+        UsersStore.set(response.data.users)
+        metadata = response.data.metadata
+        console.log($UsersStore);
+      
+
+    }
+
+  function setUser(userId){
+    console.log(userId);
+  }
+ 
+
 </script>
 
-<div class="dropdown">
+<div class="dropdown is-active">
     <div class="dropdown-trigger">
-      <Input on:keyup type="text" icon="search"/>
+      <input aria-haspopup="true" aria-controls="dropdown-menu"
+        bind:value on:keyup={getUsers}
+      />
     </div>
-    <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+    <div class={`${$UsersStore.length > 0 && 'dropdown-menu'}`} id="dropdown-menu" role="menu">
       <div class="dropdown-content">
-        <a href="#" class="dropdown-item">
-          Overview
+        {#each $UsersStore as User (User._id) }
+        <a href="#" class="dropdown-item" on:click={setUser(User._id)}>
+          {User.name}
         </a>
-        <a href="#" class="dropdown-item">
-          Modifiers
-        </a>
-        <a href="#" class="dropdown-item">
-          Grid
-        </a>
-        <a href="#" class="dropdown-item">
-          Form
-        </a>
-        <a href="#" class="dropdown-item">
-          Elements
-        </a>
-        <a href="#" class="dropdown-item">
-          Components
-        </a>
-        <a href="#" class="dropdown-item">
-          Layout
-        </a>
-        <hr class="dropdown-divider">
-        <a href="#" class="dropdown-item">
-          More
-        </a>
+        {/each}
       </div>
     </div>
   </div>
