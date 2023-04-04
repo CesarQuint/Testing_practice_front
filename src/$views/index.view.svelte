6abcf0abdb42f387f66598c8cfc2fe40bc54bdbf
CesarQuint <pedrocesarquintero001@gmail.com>
2023-04-04 09:33:21 -0600

    Working in Index and LogIn

--- src/$views/index.view.svelte
+++ src/$views/index.view.svelte
@@ -6,9 +6,18 @@
 
 </script>
 
+<style>
+    div.container.bg-logIn{
+        background-image: url(../../public/building.jpg);
+    }
+</style>
+
 <div class="container">
-    <div class="title">Index Page</div>
-    <div class="subtitle">
-        <Button on:click={() => navigateTo('login')} text="Login" icon="user" />
-    </div>
+    <div class="card">
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+          </div>
+        </div>
+      </div>
 </div>
